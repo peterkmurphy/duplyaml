5c1b77330bcc58c2633aa5d25a70cb41e392eaa1
Peter Murphy <peterkmurphy@gmail.com>
2015-12-05 16:46:16 +1000

    Added setup and YAML graph (but before testing)

---
+++ duplyaml/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# The duplyaml YAML processor.
+# The duplyaml/__init__.py file.
+# The module initialiser - used to access other parts of the duplyaml
+# package.
+
+from .ygraph import YAMLNode, YAMLScalarNode, YAMLSeqNode, YAMLMapNode, YAMLGraph
---
+++ duplyaml/ygraph.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# The duplyaml YAML processor.
+# The duplyaml/ygraph.py file.
+# Represents the YAML Representation Graph.
+
+
+# These are the constants used to define the Node kinds used with YAML nodes
+
+YAMLNODE_DEF = 0 # Used with the YAMLNode class by default.
+YAMLNODE_SCA = 1 # Used with scalar nodes.
+YAMLNODE_SEQ = 2 # Used with sequence nodes.
+YAMLNODE_MAP = 3 # Used with mapping nodes.
+
+
+# The yappystgraph represents a YAML stream: "a sequence of disjoint directed graphs, each with a root node."
+# We treat this object differently from normal nodes.
+
+class YAMLGraph:
+    """Represents a YAML representation graph."""
+
+    def __init__(self, src):
+        """ Initializes a YAML representation graph
+        :param src: The source of the data. This is implementation dependent.
+        """
+        self.src = src
+        self.children = []
+
+    def add_doc(self, node):
+        """ Adds a node as a new document to the representation graph.
+        :param node: The node to add.
+        """
+        self.children.extend(node)
+        node.graph = self
+
+    def __len__(self):
+        return len(self.children)
+
+
+class YAMLNode:
+    """ Represents a node in a YAML document. """
+
+    def __init__(self, tag, anchor=None, graph=None, kind=YAMLNODE_DEF):
+        """ Initialises a new YAML node
+        :param tag: Indicates the types of data - string, integer, etc.
+        :param anchor: Indicates the anchor used for the node, if any.
+        :param graph: Indicates the YAML representation graph containing it, if any.
+        :param kind: The kind - scalar, sequence or mapping.
+        """
+        self.tag = tag
+        self.anchor = anchor
+        self.graph = graph
+        self.kind = kind
+
+
+class YAMLScalarNode(YAMLNode):
+    """ Represents YAML nodes for scalar data: strings, integers, floats, etc. """
+    def __init__(self, canvalue, tag, anchor=None, graph=None, kind=YAMLNODE_SCA):
+        """ Initialise a new YAML scalar node
+        :param canvalue: This is the canonical value (and should be a string).
+        """
+        YAMLNode.__init__(self, tag, anchor, graph, kind)
+        self.canvalue = canvalue
+
+
+class YAMLSeqNode(YAMLNode):
+    """ Represents YAML nodes for sequences: """
+    def __init__(self, nodeseq, tag, anchor=None, graph=None, kind=YAMLNODE_SEQ):
+        """ Initialise a new YAML sequence node
+        :param nodeseq: A sequence of nodes).
+        """
+        YAMLNode.__init__(self, tag, anchor, graph, kind)
+        self.nodeseq = nodeseq
+
+    def addnode(self, node):
+        """ Add a new node to the sequence.
+        :param node: The node to be added.
+        """
+        self.nodeseq.append(node)
+        node.graph = self.graph
+
+
+class YAMLMapNode(YAMLNode):
+    """ Represents YAML nodes for mappings: """
+    def __init__(self, keyseq, valseq, tag, anchor=None, graph=None, kind=YAMLNODE_MAP):
+        """ Initialise a new YAML mapping node
+        :param keyseq: A sequence of nodes representing keys.
+        :param valseq: A sequence of nodes representing matching values.
+        """
+        YAMLNode.__init__(self, tag, anchor, graph, kind)
+        self.keyseq = keyseq
+        self.valseq = valseq
+
+    def addkvpair(self, nodekey, nodeval):
+        """ Add a new node to the sequence.
+        :param nodekey: The key to be added.
+        :param nodeval: The value to be added.
+        """
+        self.keyseq.append(nodekey)
+        nodekey.graph = self.graph
+        self.valseq.append(nodeval)
+        nodeval.graph = self.graph
