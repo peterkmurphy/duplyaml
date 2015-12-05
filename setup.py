84557fe959ea72e8183f31fd231b1829c4f7e9aa
Peter Murphy <peterkmurphy@gmail.com>
2015-12-05 16:46:52 +1000

    Added setup and YAML graph (but before testing)

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# The duplyaml YAML processor.
+# The setup.py file.
+# Used for setting up and installing duplyaml.
+
+
+from setuptools import setup
+
+setup(name='duplyaml',
+      version='0.1',
+      description='A YAML processor for Python',
+      url='https://github.com/peterkmurphy/duplyaml',
+      author='Peter Murphy',
+      author_email='peterkmurphy@gmail.com',
+      license='MIT',
+      packages=['duplyaml'],
+      zip_safe=False)
